ed3ed4fea6c54a7680119255f869f8820a319ab7
Timin29-07 <votoanphu3769@gmail.com>
2026-08-05 21:15:30 +0700

    Fix Alembic heads after merge

---
+++ backend/alembic/versions/0016_merge_auth_and_booking_heads.py
@@ -0,0 +1,18 @@
+"""merge authentication and booking feature migration heads
+
+Revision ID: 0016_merge_heads
+Revises: 0012_user_verification, 0015_branch_combos
+"""
+
+revision = "0016_merge_heads"
+down_revision = ("0012_user_verification", "0015_branch_combos")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
